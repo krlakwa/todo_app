f0307b0422da04330e4685653f9bedb041c0528c
Krzysztof Łakwa <fenrir14@gmail.com>
2018-06-07 12:12:10 +0200

    Add styles for Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,6 @@
 import React from 'react';
-const test = (id, remove) => {
-    console.log(id);
-    remove(id);
-}
-const Todo = props => <li onClick={() => test(props.id, props.remove)}>{props.text}</li>
+import style from './Todo.css'
+
+const Todo = props => <li className={style.Todo} onClick={() => props.remove(props.id)}>{props.text}</li>
 
 export default Todo;
